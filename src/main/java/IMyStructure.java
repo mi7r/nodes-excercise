910d12f73acd3c607f7239fc3f9f73a3a24d34c6
r <michalski.rados@gmail.com>
2019-10-13 23:12:02 +0200

    excercise classes added

---
+++ src/main/java/IMyStructure.java
@@ -0,0 +1,5 @@
+public interface IMyStructure {
+    INode findByCode(String code);
+    INode findByRenderer(String renderer);
+    int count();
+}
---
+++ src/main/java/MyStructure.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+public class MyStructure implements IMyStructure {
+
+    private List<INode> nodes;
+
+    public INode findByCode(String code) {
+        return null;
+    }
+
+    public INode findByRenderer(String renderer) {
+        return null;
+    }
+
+    public int count() {
+        return 0;
+    }
+}
