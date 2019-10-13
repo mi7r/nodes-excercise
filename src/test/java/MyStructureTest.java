59b1cff14d78b70b6ecffba495b5a492ae912b5d
r <michalski.rados@gmail.com>
2019-10-14 00:27:12 +0200

    new test methods added in MyStructureTest class.

--- src/test/java/MyStructureTest.java
+++ src/test/java/MyStructureTest.java
@@ -7,8 +7,7 @@ class MyStructureTest {
 
     private static final Node NODE_1 = new Node("code1","renderer1");
     private static final Node NODE_2 = new Node("code2","renderer2");
-    private static final CompositeNode COMPOSITE_NODE_1 = new CompositeNode("composite node 1","composite renderer1");
-    private static final CompositeNode COMPOSITE_NODE_2 = new CompositeNode("composite node 2","composite renderer2");
+    private static final CompositeNode COMPOSITE_NODE_1 = new CompositeNode("composite code 1","composite renderer 1");
 
     private MyStructure myStructure;
 
@@ -28,12 +27,54 @@ class MyStructureTest {
     }
 
     @Test
+    void shouldReturnNullWhenNotFoundByCode(){
+        assertNull(myStructure.findByCode("code3000"));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenPassedCodeIsNull(){
+        assertThrows(IllegalArgumentException.class,() -> myStructure.findByCode(null));
+    }
+
+    @Test
     void shouldReturnNodeFoundByRenderer() {
         assertEquals(NODE_2, myStructure.findByRenderer("renderer2"));
     }
 
     @Test
+    void shouldReturnNullWhenNotFoundByRenderer(){
+        assertNull(myStructure.findByRenderer("renderer4000"));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenPassedRendererIsNull(){
+        assertThrows(IllegalArgumentException.class, ()-> myStructure.findByRenderer(null));
+    }
+
+    @Test
+    void shouldReturnCompositeNodeFoundByRenderer(){
+        assertEquals(COMPOSITE_NODE_1, myStructure.findByRenderer("composite renderer 1"));
+    }
+
+    @Test
+    void shouldReturnCompositeNodeFoundByCode(){
+        assertEquals(COMPOSITE_NODE_1, myStructure.findByCode("composite code 1"));
+    }
+
+    @Test
     void shouldCountAllThreeAddedNodes() {
         assertEquals(3, myStructure.count());
+    }
+
+    @Test
+    void shouldReturnZeroWhenThereIsNoNodes(){
+        myStructure.getNodes().clear();
+
+        assertEquals(0, myStructure.count());
+    }
+
+    @Test
+    void shouldBeAbleToInstantiateClass(){
+        assertNotNull(myStructure);
     }
 }
